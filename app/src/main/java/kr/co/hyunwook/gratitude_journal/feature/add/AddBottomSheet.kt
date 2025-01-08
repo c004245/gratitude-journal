0b146d4e770e22655a74015637f18f148974f6f0
c004245 <c004245@naver.com>
2025-01-08 23:30:03 +0900

    chore: save logic ongoing

--- app/src/main/java/kr/co/hyunwook/gratitude_journal/feature/add/AddBottomSheet.kt
+++ app/src/main/java/kr/co/hyunwook/gratitude_journal/feature/add/AddBottomSheet.kt
@@ -14,11 +14,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -56,8 +58,22 @@ fun BottomSheetContent(onSaveGratitude: (GratitudeRecord) -> Unit) {
                     }
             )
         }
-        Box(
-            modifier = Modifier.fillMaxSize()
-        )
+
+        Button(
+            onClick = {
+                val record = GratitudeRecord(
+                    
+                )
+                onSaveGratitude
+
+            },
+        ) {
+            Text(
+                text = stringResource(id = R.string.text_splash),
+                style = GratitudeTheme.typography.regular,
+                color = Color.White,
+                fontSize = 14.sp
+            )
+        }
     }
 }
