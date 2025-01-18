94f9735f9d99353c5d3dad918246f41faa41df91
c004245 <c004245@naver.com>
2025-01-19 02:04:07 +0900

    chore: text field ongoing

--- app/src/main/java/kr/co/hyunwook/gratitude_journal/feature/add/AddBottomSheet.kt
+++ app/src/main/java/kr/co/hyunwook/gratitude_journal/feature/add/AddBottomSheet.kt
@@ -14,10 +14,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -37,8 +41,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -118,58 +124,44 @@ fun GratitudeTextField(gratitudeText: String, onTextChange: (String) -> Unit) {
             .background(black46, shape = RoundedCornerShape(15.dp)).padding(15.dp)
     ) {
 
-        Column(
-            modifier = Modifier.fillMaxWidth()
-                .padding(8.dp)
-                .clip(BalloonShape())
-                .background(black46),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
             // 텍스트 필드
-            OutlinedTextField(
-                onValueChange = {
-                },
-                value = gratitudeText,
-                placeholder = {
-                    Text(
-                        text = "오늘 감사한 내용을 적어보이",
-                        color = Color.LightGray,
-                    )
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .onFocusChanged { focusState ->
-                        isFocused = focusState.isFocused
-                    },
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = Color.Transparent, // 내부 테두리 제거
-                    unfocusedBorderColor = Color.Transparent, // 내부 테두리 제거
-                    cursorColor = Color.Black,
-                    focusedTextColor = Color.Black,
-                    unfocusedTextColor = Color.Black,
-                    disabledTextColor = Color.Gray
-                )
-            )
-        }
+        BasicTextField(
+            value = gratitudeText,
+            onValueChange = onTextChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
 
-        // 작성 완료 버튼
-        if (isFocused && gratitudeText.isNotEmpty()) {
-            Button(
-                onClick = {
-                    // 완료 버튼 클릭 로직
+                .onFocusChanged { focusState ->
+                    isFocused = focusState.isFocused
                 },
-                shape = RoundedCornerShape(16.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFFFFEB3B) // 노란색
-                ),
-                modifier = Modifier.padding(end = 8.dp, bottom = 8.dp)
-            ) {
-                Text(
-                    text = "작성 완료",
-                    color = Color.Black,
-                )
+            textStyle = TextStyle(
+                color = Color.White,
+                fontSize = 14.sp,
+            ),
+            cursorBrush = SolidColor(Color.White),
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier = Modifier.wrapContentHeight().fillMaxWidth().padding(0.dp),
+                    contentAlignment = Alignment.TopStart // 텍스트 위치를 좌측 상단에 고정
+                ) {
+                    if (gratitudeText.isEmpty() && !isFocused) {
+                        // 힌트 표시
+                        Text(
+                            text = "말풍선을 클릭해서 내용을 작성해요.",
+                            color = Color.White,
+                            style = GratitudeTheme.typography.regular,
+                            fontSize = 14.sp // 힌트의 글꼴 크기
+                        )
+                    }
+                    Box(
+                        modifier = Modifier.padding(15.dp) // 텍스트 입력 필드에도 동일한 패딩 추가
+                    ) {
+                        innerTextField() // 실제 입력 필드
+                    } // 실제 입력 필드
+                }
             }
-        }
+        )
     }
 }
 
